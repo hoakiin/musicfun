75b5be42735fe94687bf5d07c8e2a93b7fb0b112
hoakiin <millychak89@gmail.com>
2026-04-07 14:26:20 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app/ui/App/App.tsx";
+import "./index.css";
+import {Provider} from 'react-redux'
+import { store } from "./app/model/store.ts";
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+)
