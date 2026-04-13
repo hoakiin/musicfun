0f3e0d790fae427b58231cb8b13c4c862e69d149
hoakiin <millychak89@gmail.com>
2026-04-13 18:28:52 +0300

    feat: add react-tostify

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/common/components";
 import { Routing } from "@/common/routing";
+import { ToastContainer } from "react-toastify";
 import s from "./App.module.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className={s.layout}>
         <Routing />
       </div>
+      <ToastContainer />
     </>
   );
 }
