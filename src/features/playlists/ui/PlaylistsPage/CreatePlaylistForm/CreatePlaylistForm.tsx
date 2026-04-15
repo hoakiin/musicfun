85f6a6b1396b5d1cfc7451011c0097d7a1fbdcae
hoakiin <millychak89@gmail.com>
2026-04-15 18:54:11 +0300

    fix: return on first page when creating a new playlist

--- src/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx
+++ src/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx
@@ -2,7 +2,11 @@ import { useCreatePlaylistMutation } from "@/features/playlists/api/playlistsApi
 import type { CreatePlaylistFormValues } from "@/features/playlists/api/playlistsApi.types";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
-export const CreatePlaylistForm = () => {
+type Props = {
+  setCurrentPage: (page: number) => void;
+};
+
+export const CreatePlaylistForm = ({setCurrentPage}: Props) => {
   const { register, handleSubmit, reset } = useForm<CreatePlaylistFormValues>();
 
   const [createPlaylist] = useCreatePlaylistMutation();
@@ -12,6 +16,7 @@ export const CreatePlaylistForm = () => {
       .unwrap()
       .then(() => {
         reset();
+        setCurrentPage(1)
       });
   };
 
