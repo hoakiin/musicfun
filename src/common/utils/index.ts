9d1bf3b9039a9106b2ffaf157a9410e54e46107e
hoakiin <millychak89@gmail.com>
2026-04-15 22:07:20 +0300

    feat: add error utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,1 +1,6 @@
+export { handleErrors } from "./handleErrors";
 export { getPaginationPages } from "./getPaginationPages";
+export { isErrorWithProperty } from "./isErrorWithProperty";
+export { isErrorWithDetailArray } from "./isErrorWithDetailArray";
+export { trimToMaxLength } from "./trimToMaxLength";
+export { errorToast } from "./errorToast";
