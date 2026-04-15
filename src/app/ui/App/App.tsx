a8d9fd73ffcacac0aab8a558f05a18ab03cd5c4f
hoakiin <millychak89@gmail.com>
2026-04-15 18:53:21 +0300

    feat: add global LinearProgress

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -1,12 +1,16 @@
-import { Header } from "@/common/components";
+import { Header, LinearProgress } from "@/common/components";
 import { Routing } from "@/common/routing";
 import { ToastContainer } from "react-toastify";
 import s from "./App.module.css";
+import { useGlobalLoading } from "@/common/hooks";
 
 function App() {
+  const isGlobalLoading = useGlobalLoading();
+
   return (
     <>
       <Header />
+      {isGlobalLoading && <LinearProgress />}
       <div className={s.layout}>
         <Routing />
       </div>
