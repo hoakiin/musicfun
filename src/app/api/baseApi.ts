03399be9f60ebe5eda8752702d03adbc70fb35a2
hoakiin <millychak89@gmail.com>
2026-04-22 17:32:13 +0300

    add baseQueryWithReauth with mutex

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,31 +1,9 @@
-import {
-  handleErrors
-} from "@/common/utils";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithReauth } from "./baseQueryWithReauth";
 
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  tagTypes: ["Playlist"],
-  baseQuery: async (args, api, extraOptions) => {
-    const result = await fetchBaseQuery({
-      baseUrl: import.meta.env.VITE_BASE_URL,
-      headers: {
-        "API-KEY": import.meta.env.VITE_API_KEY,
-      },
-      prepareHeaders: (headers) => {
-        headers.set(
-          "Authorization",
-          `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-        );
-        return headers;
-      },
-    })(args, api, extraOptions);
-
-    if (result.error) {
-      handleErrors(result.error);
-    }
-    
-    return result;
-  },
+  reducerPath: 'baseApi',
+  tagTypes: ['Playlist', 'Auth'],
   endpoints: () => ({}),
-});
+  baseQuery: baseQueryWithReauth,
+})
