e4a6f3b21933c84bd5c677e665c9f63b3d166019
hoakiin <millychak89@gmail.com>
2026-04-22 20:05:41 +0300

    remove form for creating playlists

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -34,7 +34,6 @@ export const PlaylistsPage = () => {
   return (
     <div className={s.container}>
       <h1>Playlists page</h1>
-      <CreatePlaylistForm setCurrentPage={setCurrentPage} />
       <input
         type="search"
         placeholder={"Search playlist by title"}
