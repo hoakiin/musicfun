f31dcacbd7c601ab1f70e89e0759ba703e209a8c
hoakiin <millychak89@gmail.com>
2026-06-18 20:56:00 +0300

    feat: add useClickOutside for selects

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useDebounceValue } from "./useDebounceValue";
 export { useInfiniteScroll } from "./useInfiniteScroll";
 export { useGlobalLoading } from "./useGlobalLoading";
+export { useClickOutside } from "./useClickOutside";
