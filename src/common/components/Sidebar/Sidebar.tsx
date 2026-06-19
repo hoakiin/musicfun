7d78c92da0f977efac202ce0690cb304efa18b7e
hoakiin <millychak89@gmail.com>
2026-06-19 17:45:19 +0300

    feat: add icons for sidebar

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,16 @@
+import { Fragment } from "react";
 import { Path } from "@/common/routing";
 import { NavLink } from "react-router";
+import { Icon } from "../Icon/Icon";
 import s from "./Sidebar.module.css";
 
 const navItems = [
-  { to: Path.Main, label: "Home" },
-  { to: Path.Library, label: "Your Library" },
-  { to: Path.CreatePlaylist, label: "Create Playlist" },
-  { to: Path.UploadTrack, label: "Upload Track" },
-  { to: Path.Tracks, label: "All Tracks" },
-  { to: Path.Playlists, label: "All Playlists" },
+  { to: Path.Main, label: "Home", iconId: "home" },
+  { to: Path.Library, label: "Your Library", iconId: "library" },
+  { to: Path.CreatePlaylist, label: "Create Playlist", iconId: "create" },
+  { to: Path.UploadTrack, label: "Upload Track", iconId: "upload" },
+  { to: Path.Tracks, label: "All Tracks", iconId: "tracks" },
+  { to: Path.Playlists, label: "All Playlists", iconId: "playlists" },
 ];
 
 export const Sidebar = () => {
@@ -16,17 +18,23 @@ export const Sidebar = () => {
     <aside className={s.container}>
       <nav>
         <ul className={s.list}>
-          {navItems.map((item) => (
-            <li key={item.to}>
-              <NavLink
-                to={item.to}
-                className={({ isActive }) =>
-                  `${s.link} ${isActive ? s.activeLink : ""}`
-                }
-              >
-                {item.label}
-              </NavLink>
-            </li>
+          {navItems.map((item, index) => (
+            <Fragment key={item.to}>
+              <li>
+                <NavLink
+                  to={item.to}
+                  className={({ isActive }) =>
+                    `${s.link} ${isActive ? s.activeLink : ""}`
+                  }
+                >
+                  <Icon iconId={item.iconId} width="26" height="26" viewBox="0 0 32 32" />
+                  {item.label}
+                </NavLink>
+              </li>
+              {(index + 1) % 2 === 0 && index !== navItems.length - 1 && (
+                <li className={s.divider} />
+              )}
+            </Fragment>
           ))}
         </ul>
       </nav>
