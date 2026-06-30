fdfd885cf753de72a30c8c6f69e8c0062f2b7252
hoakiin <millychak89@gmail.com>
2026-06-30 15:58:53 +0300

    feat: add profile picture icon, add icons for dropdown menu

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Path } from "@/common/routing";
 import { useGetMeQuery, useLogoutMutation } from "@/features/auth/api/authApi";
 import { Login } from "@/features/auth/ui/Login/Login";
-import { Link, useLocation } from "react-router";
+import { Link } from "react-router";
 import s from "./Header.module.css";
 import { useState, useRef, useEffect } from "react";
 
@@ -33,17 +33,29 @@ export const Header = () => {
             className={s.loginBtn}
             onClick={() => setIsOpen((prev) => !prev)}
           >
+            <div className={s.avatar}>{data.login[0].toUpperCase()}</div>
             {data.login}
+            <svg className={s.arrow} width="10" height="6" viewBox="0 0 10 6">
+              <path d="M0 0L5 6L10 0H0Z" fill="currentColor" />
+            </svg>
           </button>
           {isOpen && (
             <ul className={s.dropdown}>
               <li>
                 <Link to={Path.Profile} onClick={() => setIsOpen(false)}>
+                  <svg className={s.icon} width="20" height="20" viewBox="0 0 24 24">
+                    <use href="#profile" />
+                  </svg>
                   My profile
                 </Link>
               </li>
               <li>
-                <button onClick={logoutHandler}>Logout</button>
+                <button onClick={logoutHandler}>
+                  <svg className={s.icon} width="20" height="20" viewBox="0 0 24 24">
+                    <use href="#logout" />
+                  </svg>
+                  Logout
+                </button>
               </li>
             </ul>
           )}
