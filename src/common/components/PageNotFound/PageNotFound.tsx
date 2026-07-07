8f0f619525cea86b6b8fa3d68b0c5b6733949d21
hoakiin <millychak89@gmail.com>
2026-07-07 16:06:01 +0300

    style: NotFound page

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,12 @@
-import s from './PageNotFound.module.css'
- 
+import { Link } from "react-router";
+import s from "./PageNotFound.module.css";
+
 export const PageNotFound = () => {
   return (
-    <>
+    <div className={s.page}>
       <h1 className={s.title}>404</h1>
       <h2 className={s.subtitle}>page not found</h2>
-    </>
-  )
-}
+      <Link to="/" className={s.homeBtn}>Go to Home</Link>
+    </div>
+  );
+};
