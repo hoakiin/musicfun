57b1304e098bd0b2e1f9760b7ad5cea25209b00c
hoakiin <millychak89@gmail.com>
2026-07-08 14:30:09 +0300

    fix: configure base path for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux'
 import { store } from "./app/model/store.ts";
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/musicfun">
     <Provider store={store}>
       <App />
     </Provider>
