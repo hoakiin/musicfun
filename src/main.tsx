fdcbf60df530a436a37c45eb3ce24f9cfae1b267
hoakiin <millychak89@gmail.com>
2026-07-10 15:11:21 +0300

    chore: configure project for Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux'
 import { store } from "./app/model/store.ts";
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/musicfun">
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
